e50ea3b47c2977aeebd76ff990b862d3db5d3d8f
Onurtag <Onurtag@users.noreply.github.com>
2024-01-09 04:12:07 +0300

    v1.1.22

--- YtBetterNotifications.user.js
+++ YtBetterNotifications.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            YtBetterNotifications (Alpha)
 // @namespace       Yt.Better.Notifications
-// @version         1.1.21
+// @version         1.1.22
 // @description     A new youtube desktop notifications panel with extra functionality.
 // @author          Onurtag
 // @match           https://www.youtube.com/new*
